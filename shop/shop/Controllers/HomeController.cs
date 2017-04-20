650b7125a2e195a4968c8e3c57f4268ee8aff595
kyrademons <mrkeni96@gmail.com>
2017-04-20 23:32:34 +0700

    lần đầu commit
    Lên sản phẩm, csdl

---
+++ shop/shop/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using shop.Models.BUS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace shop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var SP = SanPhamBUS.DanhSach();
+            return View(SP);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ shop/shop/Models/BUS/SanPhamBUS.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using web;
+
+namespace shop.Models.BUS
+{
+    public class SanPhamBUS
+    {
+        public static IEnumerable<sanpham> DanhSach()
+        {
+            var db = new webDB();
+            return db.Query<sanpham>("select * from sanpham");
+
+        }
+    }
+}
